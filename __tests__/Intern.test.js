e3d1457d4b648cf9c460aa1f74b174ba7d5bedf2
Arslan Razi <arslan.razi@outlook.com>
2021-12-19 22:28:43 -0500

    fixed the test scripts

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,19 +1,19 @@
 const Intern = require('../lib/Intern');
 
 test('add Intern school', () => {
-  const intern = new Intern('Omar', 1, 'o@email.com', 'UC Berkeley');
+  const intern = new Intern('Tim', 1, 'o@email.com', 'University of Toronto');
 
-  expect(intern.school).toBe('UC Berkeley');
+  expect(intern.school).toBe('University of Toronto');
 });
 
 test('getSchool', () => {
-  const intern = new Intern('Omar', 1, 'o@email.com', 'UC Berkeley');
+  const intern = new Intern('Tim', 1, 'o@email.com', 'University of Toronto');
 
-  expect(intern.getSchool()).toBe('UC Berkeley');
+  expect(intern.getSchool()).toBe('University of Toronto');
 });
 
 test('getRole', () => {
-  const intern = new Intern('Omar', 1, 'o@email.com', 'UC Berkeley');
+  const intern = new Intern('Tim', 1, 'o@email.com', 'University of Toronto');
 
   expect(intern.getRole()).toBe('Intern');
 });
